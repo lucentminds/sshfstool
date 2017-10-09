8e5f67d8eec032db41d319b9752a85bd94ba53ea
lucentminds <scott@lifecorp.com>
2017-10-09 17:48:24 -0500

    Initial file commit.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# sshfstool
+# sshfstool  
+10-09-2017  
 Handy tool kit for use with [sshfs](https://github.com/libfuse/sshfs).
+
+### Useage:
+```shell
+npm install -g git+https://github.com/lifecorp/sshfstool.git
+```
+
+Then just execute the app.
+```shell
+sshfstool
+```
---
+++ bin/sshfstool.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+/**
+ * 10-09-2017
+ * Handy tool kit for use with [sshfs](https://github.com/libfuse/sshfs).
+ * ~~ Scott Johnson
+ */
+
+/** List jshint ignore directives here. **/
+/* jshint undef: true, unused: true */
+/* jslint node: true */
+
+//var Q = require( 'q' );
+//var deferred = Q.defer();
+//var Module = module.exports = {};
+
+console.log( 'Hello, World!' );
