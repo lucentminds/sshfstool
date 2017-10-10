1adf98d9a80ea4f069153547d74fe115b2ee88de
lucentminds <scott@lifecorp.com>
2017-10-09 19:56:23 -0500

    Periodic commit.

--- bin/sshfstool.js
+++ bin/sshfstool.js
@@ -9,9 +9,50 @@
 /** List jshint ignore directives here. **/
 /* jshint undef: true, unused: true */
 /* jslint node: true */
+/* jshint esversion:6 */
+/* eslint-env es6 */
 
 //var Q = require( 'q' );
 //var deferred = Q.defer();
 //var Module = module.exports = {};
+const parseArgs = require( 'minimist' );
+const ARGS = parseArgs( process.argv );
+const fs = require( 'fs' );
+const path = require( 'path' );
+const APP_ROOT = path.resolve( __dirname, '..' );
+console.log( ARGS );
+
+(function(){
+   const DIR_HELP = APP_ROOT+'/docs/help';
+   var helpSubject, cHelpText;
+
+   helpSubject = ARGS.h || ARGS.help;
+   if( !helpSubject ){
+      return;
+   }
+
+   switch( helpSubject ) {
+   case 'mount':
+      cHelpText = fs.readFileSync( DIR_HELP+'/mount.txt', {encoding:'utf8'} );
+      break;
+
+   default:
+      cHelpText = fs.readFileSync( DIR_HELP+'/index.txt', {encoding:'utf8'} );
+   }
+
+   console.log( cHelpText );
+   process.exit( 0 );
+
+}());
+
+function loadHelp( cHelpName ) {
+   var cHelpText;
+   try {
+      cHelpText
+   }
+   catch( e ){
+      cHelpText = `Cannot load help text for "${cHelpName}"`;
+   }
+}// /loadHelp()
 
 console.log( 'Hello, World!' );
